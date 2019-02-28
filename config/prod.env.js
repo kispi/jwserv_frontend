9f814d4984b94ae09fc01124696c4c3220516f53
Chanho Kim <kispi@naver.com>
2019-02-28 15:49:09 +0900

    Change api endpoint to be domain since IP is volatile

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  API_URL: '"http://18.191.166.209:4506/v1/"',
+    NODE_ENV: '"production"',
+    API_URL: '"http://api.kispi.net:4506/v1/"',
 }
